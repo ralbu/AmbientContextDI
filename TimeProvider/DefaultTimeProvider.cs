52f9aeeaf02c515f0734ac3337502368ef6d9f99
Ruslan Albu <ruslan.albu@kurtosys.com>
2014-09-05 17:27:09 +0100

    Ambient context DI example

---
+++ TimeProvider/DefaultTimeProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeProvider
+{
+    public class DefaultTimeProvider : TimeProvider
+    {
+        public override DateTime UtcNow
+        {
+            get { return DateTime.UtcNow; }
+        }
+    }
+}
---
+++ TimeProvider/TimeProvider.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeProvider
+{
+    public abstract class TimeProvider
+    {
+        private static TimeProvider current;
+
+        static TimeProvider()
+        {
+            TimeProvider.current = new DefaultTimeProvider();
+        }
+
+        private static TimeProvider Current
+        {
+            get { return TimeProvider.current; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException("value");
+                }
+
+                TimeProvider.current = value;
+            }
+        }
+
+        public abstract DateTime UtcNow { get; }
+
+        public static void ResetToDefault()
+        {
+            TimeProvider.current = new DefaultTimeProvider();
+        }
+    }
+}
